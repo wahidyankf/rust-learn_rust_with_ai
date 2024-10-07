4bd6ddb7920aa8d94ad6a633d74bd86da133cad2
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-10-08 05:43:07 +0700

    update main.rs to facilitate modules

--- rust-concepts/src/main.rs
+++ rust-concepts/src/main.rs
@@ -1,7 +1,15 @@
 mod ch01_ownership;
 
 fn main() {
-    println!("Demonstrating Rust ownership!");
-    println!("--------------");
-    ch01_ownership::demo();
+    let args: Vec<String> = std::env::args().collect();
+    match args.get(1) {
+        Some(arg) if arg == "ch01_ownership" => {
+            println!("Demonstrating Rust ownership!");
+            println!("--------------");
+            ch01_ownership::demo();
+        }
+        _ => {
+            println!("Please specify a module to run. Example: cargo run -- ch01_ownership");
+        }
+    }
 }
